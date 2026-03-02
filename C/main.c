4a21791f4b7ef50bf1818b680a5cbcb1a3ae94a1
Roger Siqueira <129700489+RogerSiqueiraDeMoraes@users.noreply.github.com>
2026-03-02 13:20:46 -0300

    a

--- C/main.c
+++ C/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include "helpers.c"
 
 int main() {
-    
+    int options = 0;
+    printf("Options:\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\n");
+
+    while (options != 4) {
+        printf("Enter your choice: ");
+        scanf("%i", &options);
+        switch (options) {
+            case 1:
+                printf("Your balance is $1000\n");
+                break;
+            case 2:
+                printf("Deposit successful\n");
+                break;
+            case 3:
+                printf("Withdrawal successful\n");
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid option. Please try again.\n");
+        }
+    }
+
 }
