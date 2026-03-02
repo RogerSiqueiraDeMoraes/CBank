799ff69fffd9f64080bd67637be199d3921d773c
Roger Siqueira <129700489+RogerSiqueiraDeMoraes@users.noreply.github.com>
2026-03-02 13:32:59 -0300

    Refactor main function to update menu options and improve console encoding

--- C/main.c
+++ C/main.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
+#include <windows.h>
 #include "helpers.c"
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     int options = 0;
-    printf("Options:\n1. Check Balance\n2. Deposit\n3. Withdraw\n4. Exit\n");
-
-    while (options != 4) {
-        printf("Enter your choice: ");
-        scanf("%i", &options);
-        switch (options) {
-            case 1:
-                printf("Your balance is $1000\n");
-                break;
-            case 2:
-                printf("Deposit successful\n");
-                break;
-            case 3:
-                printf("Withdrawal successful\n");
-                break;
-            case 4:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid option. Please try again.\n");
-        }
-    }
-
+    printf("Opções:\n1 - Acessar conta\n2 - Criar conta\n3 - Sair\n");
 }
